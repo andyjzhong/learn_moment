fa739490aaf6047649db2938d1eebf4de17a72c8
Andy Zhong <Andy.Zhong@libertymutual.com%>
2018-05-26 08:27:38 -0400

    Added current day of week

--- src/components/moment_container.js
+++ src/components/moment_container.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Moment from 'react-moment'
 
 class MomentContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { property: '' };
+  }
 
   render() {
-    // const dateToFormat = '1976-04-19T12:59-0500'
     return (
-      // <Moment>{dateToFormat}</Moment>
-      <div>Should work</div>
+      <div>
+        <span>Today is: </span>
+        <Moment format="dddd"></Moment>
+      </div>
     );
   }
 }
