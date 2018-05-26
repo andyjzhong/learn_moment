25dde6ba6cd8233c6d5086b224e0775d4ffcc8b7
Andy Zhong <Andy.Zhong@libertymutual.com%>
2018-05-26 07:53:05 -0400

    Added moment dependencies

---
+++ src/components/moment_container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Moment from 'react-moment'
+
+class MomentContainer extends Component {
+
+  render() {
+    // const dateToFormat = '1976-04-19T12:59-0500'
+    return (
+      // <Moment>{dateToFormat}</Moment>
+      <div>Should work</div>
+    );
+  }
+}
+
+export default MomentContainer
